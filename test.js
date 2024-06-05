4b42f1519f6bb19bc37379f78780d3c7005fc026
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-05 12:57:00 +0900

    예제 보고 시도

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 const a = `  
-https://apis.data.go.kr/6300000/eventDataService/eventDataListJson?
-serviceKey
-=
-EltmpXonrachRZNM1YPn0W60YooGFFwLoLDZ6Sn4swsk%2FisOC2CaThKf7e49KML5nC%2B%2FOkSI4wLqcOWgXP6F9g%3D%3D&`;
+https://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=EltmpXonrachRZNM1YPn0W60YooGFFwLoLDZ6Sn4swsk%2FisOC2CaThKf7e49KML5nC%2B%2FOkSI4wLqcOWgXP6F9g%3D%3D&`;
 
 
 const b = "endpoint 주소" + "주소 값";
@@ -14,12 +11,6 @@ const keyName =
 "EltmpXonrachRZNM1YPn0W60YooGFFwLoLDZ6Sn4swsk%2FisOC2CaThKf7e49KML5nC%2B%2FOkSI4wLqcOWgXP6F9g%3D%3D";
 
 const example = `
-http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?
-serviceKey=${keyName}
-&
-numOfRows=10
-&
-pageNo
-`;
+http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${keyName}&numOfRows=10&pageNo=1`;
 
 console.log(example);
