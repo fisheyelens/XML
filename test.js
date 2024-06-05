225200111a92157ce2c22458f0efee15ccd16f63
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-05 11:55:25 +0900

    encoding decoding endpoint

---
+++ test.js
@@ -0,0 +1,25 @@
+const a = `  
+https://apis.data.go.kr/6300000/eventDataService/eventDataListJson?
+serviceKey
+=
+EltmpXonrachRZNM1YPn0W60YooGFFwLoLDZ6Sn4swsk%2FisOC2CaThKf7e49KML5nC%2B%2FOkSI4wLqcOWgXP6F9g%3D%3D&`;
+
+
+const b = "endpoint 주소" + "주소 값";
+
+const endPoint = `http://apis.data.go.kr/6300000/eventDataService`;
+
+
+const keyName = 
+"EltmpXonrachRZNM1YPn0W60YooGFFwLoLDZ6Sn4swsk%2FisOC2CaThKf7e49KML5nC%2B%2FOkSI4wLqcOWgXP6F9g%3D%3D";
+
+const example = `
+http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?
+serviceKey=${keyName}
+&
+numOfRows=10
+&
+pageNo
+`;
+
+console.log(example);
